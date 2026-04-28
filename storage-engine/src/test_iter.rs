1c6684abd7d145348c2533ab89ffce270cb1aff4
A-007481D <labidabdelmalek@gmail.com>
2026-04-28 13:21:49 +0100

    Cleanup scratch files

--- storage-engine/src/test_iter.rs
+++
@@ -1,5 +0,0 @@
-use crossbeam_skiplist::map::Entry;
-use bytes::Bytes;
-pub struct MemTableIterator {
-    current: Option<Entry<Bytes, super::memtable::EntryValue>>,
-}
--- storage-engine/test_iter.rs
+++
@@ -1,5 +0,0 @@
-use crossbeam_skiplist::SkipMap;
-use bytes::Bytes;
-fn test(map: &SkipMap<Bytes, Bytes>) {
-    let _ = map.range(..);
-}
