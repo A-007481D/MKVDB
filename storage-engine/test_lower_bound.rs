942085b479fa433b9821859f9c93f1ac6e13c522
A-007481D <labidabdelmalek@gmail.com>
2026-04-28 13:21:49 +0100

    Cleanup scratch files

--- storage-engine/test_lower_bound.rs
+++
@@ -1,5 +0,0 @@
-use crossbeam_skiplist::SkipMap;
-use bytes::Bytes;
-fn test(map: &SkipMap<Bytes, Bytes>, key: &Bytes) {
-    let entry = map.lower_bound(std::ops::Bound::Excluded(key));
-}
--- storage-engine/test_send.rs
+++
@@ -1,5 +0,0 @@
-use crossbeam_skiplist::SkipMap;
-fn assert_send<T: Send>() {}
-fn main() {
-    assert_send::<crossbeam_skiplist::map::Iter<'static, i32, i32>>();
-}
